b2e8e28c5b2c828455b2d3c1e90a659a60759861
Junaid <junaid_zaki@hotmail.com>
2018-05-02 10:51:57 +0530

    clean slating master

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    'secretKey': process.env.SECRET,
-    'mongoUrl' : process.env.MONGO,
-    'facebook': {
-        clientID: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        callbackURL: 'https://localhost:3443/users/facebook/callback'
-    }
-}
